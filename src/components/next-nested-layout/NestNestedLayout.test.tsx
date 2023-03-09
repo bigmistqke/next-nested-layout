ac93d7c660db8782df516d8524a3ccb0df6f50b9
bigmistqke <vandijckv@gmail.com>
2023-03-09 16:25:23 +0100

    add todo

--- src/components/next-nested-layout/NestNestedLayout.test.tsx
+++ src/components/next-nested-layout/NestNestedLayout.test.tsx
@@ -2,38 +2,3 @@ import React from "react";
 import { render, within } from "@testing-library/react";
 
 // TODO: write tests
-
-/* import TestComponent from "./NestNestedLayout";
-import { TestComponentProps } from "./NestNestedLayout.types";
-
-describe("TestComponent", () => {
-  const renderComponent = ({ heading, content }: Partial<TestComponentProps>) =>
-    render(
-      <TestComponent
-        heading={heading || "Default heading text"}
-        content={content || <div>Default content</div>}
-      />
-    );
-
-  it("should render heading text correctly", () => {
-    const headingText = "Some test heading";
-
-    const { getByTestId } = renderComponent({ heading: headingText });
-
-    const testComponent = getByTestId("test-component__heading");
-
-    expect(testComponent).toHaveTextContent(headingText);
-  });
-
-  it("should render content correctly", () => {
-    const { getByTestId } = renderComponent({
-      content: <div data-testid="some-test-content">I am test content</div>
-    });
-
-    expect(
-      within(getByTestId("test-component__content")).queryByTestId(
-        "some-test-content"
-      )
-    ).toBeInTheDocument();
-  });
-}); */
